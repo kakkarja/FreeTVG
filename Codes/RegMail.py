7383800a3ed15e2e6bf7ca20ff99f211ec0e0848
KarjaKAK <kakkarja.github@gmail.com>
2021-03-30 08:29:36 +0700

    Update RegMail.py

--- Codes/RegMail.py
+++ Codes/RegMail.py
@@ -1,15 +1,19 @@
 import webbrowser
 from Ck_user_serialNum import RegKey
 import os
+from urllib import parse
 
 def composemail(pth: str):
     a = RegKey()
     b = a.ckuserserial()
-    c = str(a.writeck(b)).replace('&', '%26')
+    c = str(a.writeck(b))
     pth = os.path.join(pth, 'Registration.txt')
     with open(pth, 'w') as reg:
         reg.write(c)
     recipient = 'kakkarja.minder@gmail.com' 
     subject = 'REG:[TVG-Registration]'
-    body = f"This is encryption of the User and Serial number of this PC:%0A{c}%0A%0A"
-    webbrowser.open(f'mailto:?to={recipient}&subject={subject}&body={body}', new = 1)
+    body = f"This is encryption of the User and Serial number of this PC:\n{c}\n\n"
+    webbrowser.open(f'mailto:{recipient}?subject={subject}&body={parse.quote(body)}', new = 1)
+
+if __name__ == '__main__':
+    composemail(os.getcwd())
